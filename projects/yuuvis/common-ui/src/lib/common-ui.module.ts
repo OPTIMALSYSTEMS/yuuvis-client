c277ea8bc1acfae477f762d3c6ca5ef5e7f0a606
Frank Wille <f.wille@optimal-systems.de>
2019-08-26 17:17:37 +0200

    additional styling fixing

--- projects/yuuvis/common-ui/src/lib/common-ui.module.ts
+++ projects/yuuvis/common-ui/src/lib/common-ui.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
 import { SidebarModule } from 'primeng/sidebar';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { EmptyComponent } from './components/empty/empty.component';
 import { IconComponent } from './components/icon/icon.component';
 import { IconService } from './components/icon/icon.service';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -11,8 +12,8 @@ import { FocusWithinDirective } from './directives/focus-within.directive';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule, AccordionModule, SidebarModule],
-  declarations: [IconComponent, FocusWithinDirective, AccordionComponent, SidebarComponent],
-  exports: [IconComponent, FocusWithinDirective, AccordionComponent, SidebarComponent],
+  declarations: [IconComponent, FocusWithinDirective, AccordionComponent, SidebarComponent, EmptyComponent],
+  exports: [IconComponent, FocusWithinDirective, AccordionComponent, SidebarComponent, EmptyComponent],
   providers: [IconService]
 })
 export class YuvCommonUiModule {}
