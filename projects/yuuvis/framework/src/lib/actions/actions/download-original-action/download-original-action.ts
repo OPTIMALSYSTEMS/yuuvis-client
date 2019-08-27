01dfdeee5baeee637daeeaef15310193b3ebdc81
Manuel Ansorg <ansorg@optimal-systems.de>
2019-08-27 10:45:45 +0200

    COOL-10252 Download-Action nutzt nun download-funktion des backend-services
    Styling

--- projects/yuuvis/framework/src/lib/actions/actions/download-original-action/download-original-action.ts
+++ projects/yuuvis/framework/src/lib/actions/actions/download-original-action/download-original-action.ts
@@ -1,9 +1,7 @@
 
-import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { BackendService, DmsObject, TranslateService } from '@yuuvis/core';
-import * as FileSaver from 'file-saver';
 import { Observable, of as observableOf, of } from 'rxjs';
 import { DmsObjectTarget } from '../../action-target';
 import { SimpleAction } from '../../interfaces/action.interface';
@@ -33,56 +31,7 @@ export class DownloadOriginalActionComponent extends DmsObjectTarget implements 
   };
 
   run(selection: DmsObject[]): Observable<boolean> {
-    const isVersionState = this.router.url.startsWith('/versions');
-    // this.backend.downloadContent(selection, null, isVersionState);
-    this.downloadContent(selection);
+    this.backend.downloadContent(selection.map(o => o.id));
     return of(true);
-  }
-
-  /**
-   * Download the content of dms objects.
-   *
-   * @param DmsObject[] dmsObjects Array of dms objects to be downloaded
-   * @param "PDF" | "TIFF" | "TEXT" | "JPEG" rendition The type of rendition to be downloaded. If not specified, the original content will be downloaded.
-   * Possible renditions are `PDF`, `TIFF`, `TEXT`, `JPEG`.
-   */
-  private downloadContent(dmsObjects: DmsObject[], rendition?: 'PDF' | 'TIFF' | 'TEXT' | 'JPEG', fileNameWithVersionNumber?: boolean) {
-
-    dmsObjects.forEach(dmsObject => {
-      // if (dmsObject.content) {
-      if (true) {
-
-        // let uri = `${this.getServiceBase()}/dms/${dmsObject.content.id}/content?type=${dmsObject.content.type}&asdownload=true`;
-        let uri = `/api/dms/objects/${dmsObject.id}/contents/file`;
-        // if (rendition) {
-        //   uri += `&rendition=${rendition}&_intent=DOWNLOAD_${rendition}`;
-        // } else {
-        //   uri += '&intent=DOWNLOAD';
-        // }
-        // if (dmsObject.content.id === dmsObject.id) {
-        //   uri += `&version=${dmsObject.version}`;
-        // }
-        this.download(uri, fileNameWithVersionNumber ? dmsObject.version : null);
-      } else {
-        // this.logger.error('The provided dms object has no content', dmsObject);
-      }
-    });
-  }
-
-  private download(uri: string, version?: number) {
-    this.backend.get(uri, null, { observe: 'response', responseType: 'blob' })
-      .subscribe((res) => {
-        FileSaver.saveAs(
-          res.body,
-          this.getFileNameFromHttpResponse(res, version)
-        );
-      });
-  }
-
-  private getFileNameFromHttpResponse(res: HttpResponse<any>, version: number) {
-    const contentDispositionHeader = res.headers.get('Content-Disposition') || '';
-    const filename = (contentDispositionHeader.match('filename=\"(.*)\"') || [, 'unknown'])[1];
-    const index = !~filename.lastIndexOf('.') ? filename.length : filename.lastIndexOf('.');
-    return !version ? filename : `${filename.slice(0, index)}_v${version}${filename.slice(index)}`;
   }
 }
