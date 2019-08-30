2e9b52c10f0b23f70eec31691995d6a70c59dfe4
Andreas Schulz <schulz@optimal-systems.de>
2019-08-30 11:01:34 +0200

    fix

--- src-component-tests/app/test-summary/test-summary.component.ts
+++ src-component-tests/app/test-summary/test-summary.component.ts
@@ -7,18 +7,20 @@ import { DmsObject } from '@yuuvis/core';
   styleUrls: ['./test-summary.component.scss']
 })
 export class TestSummaryComponent implements OnInit {
-  private dmsObject: DmsObject = new DmsObject({
-    objectTypeId: 'email:email',
-    content: {
-      contentStreamId: 'D3C1D523-5D37-11E9-AAA5-27E7DF182E4E',
-      digest: 'F1F56858B92DC6E9242A2C03245575BE7B1FADC3D7119827BB7E117DC0D4DB46',
-      archivePath: 'kolibri/2019/04/12/',
-      fileName: '02 Neue OS-Webseiten sind LIVE.msg',
-      mimeType: 'application/vnd.ms-outlook',
-      size: 60416
-    },
-    fields: {}
-  });
+  private dmsObject: DmsObject = null;
+
+  // new DmsObject({
+  //   objectTypeId: 'email:email',
+  //   content: {
+  //     contentStreamId: 'D3C1D523-5D37-11E9-AAA5-27E7DF182E4E',
+  //     digest: 'F1F56858B92DC6E9242A2C03245575BE7B1FADC3D7119827BB7E117DC0D4DB46',
+  //     archivePath: 'kolibri/2019/04/12/',
+  //     fileName: '02 Neue OS-Webseiten sind LIVE.msg',
+  //     mimeType: 'application/vnd.ms-outlook',
+  //     size: 60416
+  //   },
+  //   fields: {}
+  // });
 
   summaryObject = this.dmsObject;
 
@@ -33,9 +35,9 @@ export class TestSummaryComponent implements OnInit {
   }
 
   invalidDmsObjectInput() {
-    this.summaryObject = {
-      p: 'something'
-    };
+    // this.summaryObject = {
+    //   p: 'something'
+    // };
   }
 
   ngOnInit() {}
