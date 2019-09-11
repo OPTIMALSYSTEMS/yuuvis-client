80d0647920d904dfb9be7f915597d785658b68fe
Andreas Schulz <schulz@optimal-systems.de>
2019-09-11 08:10:02 +0200

    fixed circular dependency issue in core

--- projects/yuuvis/core/src/lib/service/backend/backend.service.ts
+++ projects/yuuvis/core/src/lib/service/backend/backend.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DmsObject } from '@yuuvis/core';
 import { Observable } from 'rxjs';
+import { DmsObject } from '../../model/dms-object.model';
 import { ConfigService } from '../config/config.service';
 import { Logger } from '../logger/logger';
 import { ApiBase } from './api.enum';
