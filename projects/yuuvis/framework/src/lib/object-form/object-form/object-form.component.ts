f78ab139db4b5920b1e97fa1c53f457e3452cf5f
Frank Wille <f.wille@optimal-systems.de>
2019-09-16 13:28:26 +0200

    COOL-10187 exclude tabels from summary

--- projects/yuuvis/framework/src/lib/object-form/object-form/object-form.component.ts
+++ projects/yuuvis/framework/src/lib/object-form/object-form/object-form.component.ts
@@ -190,7 +190,7 @@ export class ObjectFormComponent extends UnsubscribeOnDestroy implements OnDestr
 
   initValidators(form) {
     if (this.form) {
-      for (let key in form.controls) {
+      for (const key of Object.keys(form.controls)) {
         const control = form.controls[key];
         if (control.controls) {
           this.initValidators(control);
@@ -525,7 +525,11 @@ export class ObjectFormComponent extends UnsubscribeOnDestroy implements OnDestr
    * @param formElement - form element object
    */
   private getValidators(formElement: any): ValidatorFn[] {
-    let elmValidators = FormValidation.getValidators(formElement, this.formOptions.formModel.situation);
+    let elmValidators = [];
+
+    if (formElement.type !== 'BOOLEAN') {
+      elmValidators = FormValidation.getValidators(formElement, this.formOptions.formModel.situation);
+    }
 
     // add custom validator for script enabled forms
     if (this.scriptingScope) {
