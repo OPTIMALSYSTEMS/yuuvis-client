d6f49041d5e8b77d905d283cad51edee20948c9d
Andreas Schulz <schulz@optimal-systems.de>
2019-09-24 07:38:40 +0200

    dark theme styling

--- src/app/states/settings/settings.component.ts
+++ src/app/states/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ConfigService, TranslateService, UserService, YuvUser } from '@yuuvis/core';
 import { Observable } from 'rxjs';
+import { AppCacheService } from './../../../../projects/yuuvis/core/src/lib/service/cache/app-cache.service';
 
 @Component({
   selector: 'yuv-settings',
@@ -9,16 +10,26 @@ import { Observable } from 'rxjs';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  // private STORAGE_KEY = 'yuv.tmp.settings'
   user$: Observable<YuvUser>;
   clientLocales: any;
 
-  constructor(private translate: TranslateService, private titleService: Title, public config: ConfigService, private userService: UserService) {
+  constructor(
+    private translate: TranslateService,
+    private appCache: AppCacheService,
+    private titleService: Title,
+    public config: ConfigService,
+    private userService: UserService
+  ) {
     this.clientLocales = config.getClientLocales();
+    // this.appCache.getItem(this.STORAGE_KEY).subscribe
   }
 
   changeClientLocale(iso: string) {
     this.userService.changeClientLocale(iso);
   }
+
+  toggleDarkMode() {}
 
   ngOnInit() {
     this.titleService.setTitle(this.translate.instant('yuv.client.state.settings.title'));
