1cdeb0ab225c33d229f5d9dba59ca0c9e979cd8d
Andreas Schulz <schulz@optimal-systems.de>
2019-09-27 08:51:44 +0200

    dark styles for forms

--- src-component-tests/app/app.component.ts
+++ src-component-tests/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService, YuvUser } from '@yuuvis/core';
+import { AppCacheService, UserService, YuvUser } from '@yuuvis/core';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +10,40 @@ import { UserService, YuvUser } from '@yuuvis/core';
 export class AppComponent implements OnInit {
   routes = [];
   user: YuvUser;
+  darkMode: boolean;
+
+  private STORAGE_KEY = 'yuv.cmp-test.settings';
 
   @HostBinding('class.showNav') showNav: boolean;
-  @HostBinding('class.dark') darkMode: boolean;
+  // @HostBinding('class.dark') darkMode: boolean;
 
-  constructor(private router: Router, private userService: UserService) {
+  constructor(private router: Router, private appCache: AppCacheService, private userService: UserService) {
     this.userService.user$.subscribe(u => {
       this.user = u;
     });
+    this.appCache.getItem(this.STORAGE_KEY).subscribe(res => {
+      this.darkMode = res.darkMode;
+      this.setDarkMode();
+    });
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    this.setDarkMode();
+    this.appCache
+      .setItem(this.STORAGE_KEY, {
+        darkMode: this.darkMode
+      })
+      .subscribe();
+  }
+
+  private setDarkMode() {
+    const bodyClasses = document.getElementsByTagName('body')[0].classList;
+    if (this.darkMode) {
+      bodyClasses.add('dark');
+    } else {
+      bodyClasses.remove('dark');
+    }
   }
 
   ngOnInit() {
