74fd65110b4e3960b82af416a917705360f6f2c4
Andreas Schulz <schulz@optimal-systems.de>
2019-10-21 13:23:06 +0200

    styles and app-search service

--- src/app/states/dashboard/dashboard.component.ts
+++ src/app/states/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { SearchQuery } from '@yuuvis/core';
 import { APP_VARS } from '../../app.vars';
+import { AppSearchService } from '../../service/app-search.service';
 @Component({
   selector: 'yuv-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,9 +11,17 @@ import { APP_VARS } from '../../app.vars';
   host: { class: 'themeBackground' }
 })
 export class DashboardComponent implements OnInit {
-  constructor(private router: Router, private titleService: Title) {}
+  // application wide search query
+  appQuery: SearchQuery;
+
+  constructor(private router: Router, private appSearch: AppSearchService, private titleService: Title) {
+    this.appSearch.query$.subscribe((q: SearchQuery) => {
+      this.appQuery = q;
+    });
+  }
 
   onQuickSearchQuery(query: SearchQuery) {
+    this.appSearch.setQuery(query);
     this.router.navigate(['/result'], {
       queryParams: { query: JSON.stringify(query.toQueryJson()) }
     });
