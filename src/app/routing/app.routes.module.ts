1e70d9384274622485bbbd5ce19aa81430cea9b7
Manuel Ansorg <ansorg@optimal-systems.de>
2019-10-31 01:40:16 +0100

    COOL-10863 Object state erstellt

--- src/app/routing/app.routes.module.ts
+++ src/app/routing/app.routes.module.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from '../states/about/component/about.component';
 import { DashboardComponent } from '../states/dashboard/dashboard.component';
 import { EnterComponent } from '../states/enter/enter.component';
 import { NotFoundComponent } from '../states/not-found/not-found.component';
+import { ObjectComponent } from '../states/object/object.component';
 import { ResultComponent } from '../states/result/result.component';
 import { SettingsComponent } from '../states/settings/settings.component';
 import { AuthGuard } from './auth-guard/auth-guard.service';
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
   { path: 'result', component: ResultComponent, canActivate: [AuthGuard] },
+  { path: 'object/:id', component: ObjectComponent, canActivate: [AuthGuard] },
   // default route
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   // 404 route
