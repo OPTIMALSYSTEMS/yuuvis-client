afe2532da38d7b9aee6a451e5c2d325e7b59b44e
Frank Wille <fwille@optimal-systems.de>
2019-12-02 17:18:10 +0100

    COOL-11082 upload status in appBar

--- projects/yuuvis/framework/src/lib/components/components.module.ts
+++ projects/yuuvis/framework/src/lib/components/components.module.ts
@@ -9,6 +9,7 @@ import { AngularResizedEventModule } from 'angular-resize-event';
 import { AngularSplitModule } from 'angular-split';
 import { DragScrollModule } from 'ngx-drag-scroll';
 import { DialogModule } from 'primeng/dialog';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { TabViewModule } from 'primeng/tabview';
 import { YuvDirectivesModule } from '../directives/directives.module';
@@ -25,35 +26,24 @@ import { ResponsiveMasterSlaveComponent } from './responsive-master-slave/respon
 import { ResponsiveTabContainerComponent } from './responsive-tab-container/responsive-tab-container.component';
 import { UploadProgressOverlayComponent } from './upload-progress-overlay/upload-progress-overlay.component';
 
+const components = [
+  PanelComponent,
+  DialogComponent,
+  FilePickerComponent,
+  UploadProgressOverlayComponent,
+  RecentActivitiesComponent,
+  ResponsiveDataTableComponent,
+  BusyOverlayDirective,
+  ResponsiveMasterSlaveComponent,
+  ActionMenuBarComponent,
+  ResponsiveTabContainerComponent
+];
+
 @NgModule({
-  declarations: [
-    ResponsiveDataTableComponent,
-    BusyOverlayDirective,
-    ResponsiveMasterSlaveComponent,
-    ActionMenuBarComponent,
-    ResponsiveTabContainerComponent,
-    PanelComponent,
-    DialogComponent,
-    UploadProgressOverlayComponent,
-    FilePickerComponent,
-    RecentActivitiesComponent,
-    RecentAcitivitiesItemComponent
-  ],
-  exports: [
-    ResponsiveDataTableComponent,
-    BusyOverlayDirective,
-    ResponsiveMasterSlaveComponent,
-    ActionMenuBarComponent,
-    ResponsiveTabContainerComponent,
-    TabViewModule,
-    PanelComponent,
-    ActionMenuBarComponent,
-    DialogComponent,
-    FilePickerComponent,
-    UploadProgressOverlayComponent,
-    RecentActivitiesComponent
-  ],
+  declarations: [...components, RecentAcitivitiesItemComponent],
+  exports: [...components, TabViewModule],
   imports: [
+    OverlayPanelModule,
     CommonModule,
     AngularResizedEventModule,
     CdkStepperModule,
