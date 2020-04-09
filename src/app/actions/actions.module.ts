994edf9708a0126e31f5f8897787112beae91a82
Andreas Schulz <schulz@optimal-systems.de>
2020-04-09 09:46:22 +0200

    COOL-11942 Open and version action

--- src/app/actions/actions.module.ts
+++ src/app/actions/actions.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ACTIONS } from '@yuuvis/framework';
 import { OpenContextActionComponent } from './open-context-action/open-context-action';
+import { OpenVersionsActionComponent } from './open-versions-action/open-versions-action';
 
 // Array of actions provided by the client app
-export const appActionsComponents = [OpenContextActionComponent];
+export const appActionsComponents = [OpenContextActionComponent, OpenVersionsActionComponent];
 
 @NgModule({
   declarations: appActionsComponents,
