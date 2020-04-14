eee5f7f9f53886566e6ce6b0a6165498fd681ec7
Peter Krajnik <krajnik@optimal-systems.de>
2020-04-14 10:44:30 +0200

    helpers moved to common

--- projects/yuuvis/framework/src/lib/common/index.ts
+++ projects/yuuvis/framework/src/lib/common/index.ts
@@ -2,4 +2,5 @@ export { FadeInAnimations } from './animations/fadein.animation';
 export * from './common.module';
 export { IconRegistryService } from './components/icon/service/iconRegistry.service';
 export * from './components/sidebar/sidebar.enum';
+export * from './util/helpers';
 export { UnsubscribeOnDestroy } from './util/unsubscribe.component';
