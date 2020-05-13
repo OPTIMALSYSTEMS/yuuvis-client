50767d7c51b31027c96fa9725eaa2f8700e222b2
Andreas Schulz <schulz@optimal-systems.de>
2020-05-13 09:15:42 +0200

    Review refactoring COOL-12106

--- projects/yuuvis/framework/src/lib/components/upload-progress-overlay/upload-progress-overlay.component.ts
+++ projects/yuuvis/framework/src/lib/components/upload-progress-overlay/upload-progress-overlay.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core
 import { ProgressStatus, UploadResult, UploadService } from '@yuuvis/core';
 import { OverlayPanel } from 'primeng/overlaypanel';
 import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { IconRegistryService } from '../../common/components/icon/service/iconRegistry.service';
 import { clear, done } from './../../svg.generated';
 @Component({
@@ -28,14 +29,14 @@ export class UploadProgressOverlayComponent {
 
   constructor(private uploadService: UploadService, private iconRegistry: IconRegistryService) {
     this.iconRegistry.registerIcons([clear, done]);
-    this.progressStatus$ = this.uploadService.status$;
+    this.progressStatus$ = this.uploadService.status$.pipe(
+      tap((s) => {
+        if (!s.items.length && this.uploadsOverlay) {
+          this.uploadsOverlay.hide();
+        }
+      })
+    );
     this.completedUp$ = this.uploadService.uploadStatus$;
-
-    this.progressStatus$.subscribe((s) => {
-      if (!s.items.length && this.uploadsOverlay) {
-        this.uploadsOverlay.hide();
-      }
-    });
   }
 
   openObject(item: UploadResult) {
