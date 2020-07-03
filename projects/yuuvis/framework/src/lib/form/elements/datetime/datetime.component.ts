b465227d90e9f605084f32007f38668095a57408
Manuel Ansorg <ansorg@optimal-systems.de>
2020-07-03 18:30:08 +0200

    COLL-12405 gefixt

--- projects/yuuvis/framework/src/lib/form/elements/datetime/datetime.component.ts
+++ projects/yuuvis/framework/src/lib/form/elements/datetime/datetime.component.ts
@@ -163,6 +163,7 @@ export class DatetimeComponent implements OnInit, ControlValueAccessor, Validato
   onMaskValueChange(event) {
     if (event === this._datePattern || event.length === 0) {
       this.value = null;
+      this.isValid = true;
       this.propagate();
     }
   }
