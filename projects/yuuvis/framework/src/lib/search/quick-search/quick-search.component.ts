18a436682745b1bd856126405525b0c334d43570
Peter Krajnik <krajnik@optimal-systems.de>
2020-07-28 22:02:11 +0200

    COOL-12646 quick-search: wait for debounce / form changes

--- projects/yuuvis/framework/src/lib/search/quick-search/quick-search.component.ts
+++ projects/yuuvis/framework/src/lib/search/quick-search/quick-search.component.ts
@@ -164,7 +164,8 @@ export class QuickSearchComponent implements OnInit, AfterViewInit {
   @HostBinding('class.inline') _inline: boolean;
 
   @HostListener('keydown.enter', ['$event']) onEnter(event) {
-    this.executeSearch();
+    // wait for debounce / form changes
+    setTimeout(() => this.executeSearch(), 500);
   }
 
   constructor(
