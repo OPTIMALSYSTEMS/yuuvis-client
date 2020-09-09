3e6abfe6eb2d975642a08a0167c475ff13289bf2
Frank Wille <fwille@optimal-systems.de>
2020-09-09 14:37:32 +0200

    show icon in bpm list

--- projects/yuuvis/framework/src/lib/bpm/services/formatProcessData.service.ts
+++ projects/yuuvis/framework/src/lib/bpm/services/formatProcessData.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FollowUp, InboxItem, ProcessData, Sort, SystemService, TaskData, Utils } from '@yuuvis/core';
+import { ObjectTypeIconComponent } from '../../common/components/object-type-icon/object-type-icon.component';
 import { ResponsiveTableData } from '../../components/responsive-data-table/responsive-data-table.interface';
 import { GridService } from './../../services/grid/grid.service';
 
@@ -14,17 +15,17 @@ export class FormatProcessDataService {
   // task/ => inbox State
   formatTaskDataForTable(processData: TaskData[]): ResponsiveTableData {
     return this.processDataForTable(processData.map((data) => new InboxItem(data)).sort(Utils.sortValues('expiryDateTime', Sort.DESC)), [
+      'type',
       'subject',
-      'expiryDateTime',
-      'type'
+      'expiryDateTime'
     ]);
   }
 
   formatProcessDataForTable(processData: ProcessData[]): ResponsiveTableData {
     return this.processDataForTable(processData.map((data) => new FollowUp(data)).sort(Utils.sortValues('expiryDateTime', Sort.DESC)), [
+      'type',
       'subject',
       'expiryDateTime',
-      'type',
       'businessKey',
       'startTime'
     ]);
@@ -37,6 +38,7 @@ export class FormatProcessDataService {
         headerClass: `col-header-type-${field}`,
         headerName: this.systemService.getLocalizedResource(`${field}_label`),
         ...(field.toLowerCase().includes('time') && { cellRenderer: this.gridService.dateTimeCellRenderer() }),
+        ...(field.toLowerCase() === 'type' && { cellRendererFramework: ObjectTypeIconComponent }),
         resizable: true,
         sortable: true
       })),
