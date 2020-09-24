98da67d8d08c199dde99e65a91d753753546941a
Frank Wille <fwille@optimal-systems.de>
2020-09-24 14:40:02 +0200

    COOL-13309 bpm details

--- projects/yuuvis/framework/src/lib/bpm/bpm.module.ts
+++ projects/yuuvis/framework/src/lib/bpm/bpm.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { TranslateModule, YuvCoreModule } from '@yuuvis/core';
 import { YuvCommonModule } from '../common/common.module';
 import { YuvComponentsModule } from '../components/components.module';
+import { YuvObjectDetailsModule } from '../object-details/object-details.module';
+import { ProcessDetailsComponent } from './process-details/process-details.component';
 import { ProcessListComponent } from './process-list/process-list.component';
 
+const components = [ProcessDetailsComponent, ProcessListComponent];
 @NgModule({
-  imports: [CommonModule, YuvComponentsModule, YuvCoreModule, TranslateModule, YuvCommonModule],
-  declarations: [ProcessListComponent],
-  exports: [ProcessListComponent]
+  imports: [CommonModule, YuvComponentsModule, YuvCoreModule, TranslateModule, YuvCommonModule, YuvObjectDetailsModule],
+  declarations: components,
+  exports: components
 })
 export class YuvBpmModule {}
