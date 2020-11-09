480b45b608873038e5c13e269462e9a98a294949
Peter Krajnik <krajnik@optimal-systems.de>
2020-11-09 12:21:30 +0100

    cli update to angular 10.2.2

--- projects/yuuvis/framework/src/lib/common/util/unsubscribe.component.ts
+++ projects/yuuvis/framework/src/lib/common/util/unsubscribe.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 /**
  * Use for any custom cleanup that needs to occur when the instance is destroyed.
  */
+// TODO: Add Angular decorator.
 export abstract class UnsubscribeOnDestroy implements OnDestroy {
   protected componentDestroyed$: Subject<void>;
   /**
