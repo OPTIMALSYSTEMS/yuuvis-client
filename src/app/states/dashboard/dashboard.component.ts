5952cc242dd6732e70bfad98a0ab33855422c37c
Peter Krajnik <krajnik@optimal-systems.de>
2020-11-23 17:55:12 +0100

    COOL-13722 aggregations via grouped-select to support keybord naivgation

--- src/app/states/dashboard/dashboard.component.ts
+++ src/app/states/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import { Component, HostBinding, OnInit, ViewChild } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { NavigationExtras, Router } from '@angular/router';
-import { EventService, SearchQuery, UserRoles, UserService, Utils, YuvEventType, YuvUser } from '@yuuvis/core';
-import { ObjectTypeAggregation, QuickSearchComponent, RecentItem } from '@yuuvis/framework';
+import { EventService, SearchQuery, SystemService, UserRoles, UserService, Utils, YuvEventType, YuvUser } from '@yuuvis/core';
+import { GroupedSelectComponent, ObjectTypeAggregation, QuickSearchComponent, RecentItem, Selectable, SelectableGroup } from '@yuuvis/framework';
 import { APP_VARS } from '../../app.vars';
 import { FrameService } from '../../components/frame/frame.service';
 import { AppSearchService } from '../../service/app-search.service';
@@ -13,13 +13,22 @@ import { AppSearchService } from '../../service/app-search.service';
 })
 export class DashboardComponent implements OnInit {
   @ViewChild('quickSearch') quickSearchEl: QuickSearchComponent;
+  @ViewChild('aggs') aggsEl: GroupedSelectComponent;
   // application wide search query
   appQuery: SearchQuery;
   aggs: ObjectTypeAggregation[];
+  aggsGroups: SelectableGroup[];
   disableFileDrop: boolean;
   @HostBinding('class.aggregations') hasAggs: boolean;
 
   reloadComponent = true;
+
+  @HostListener('keydown.ArrowDown', ['$event']) onKeyDown(event) {
+    if (!event.defaultPrevented && this.aggsEl) {
+      this.aggsEl.focus();
+      return false;
+    }
+  }
 
   constructor(
     private router: Router,
@@ -27,6 +36,7 @@ export class DashboardComponent implements OnInit {
     private frameService: FrameService,
     private appSearch: AppSearchService,
     private eventService: EventService,
+    private systemService: SystemService,
     private titleService: Title
   ) {
     this.userService.user$.subscribe((user: YuvUser) => {
@@ -67,10 +77,23 @@ export class DashboardComponent implements OnInit {
   onTypeAggregation(aggs: ObjectTypeAggregation[]) {
     this.aggs = aggs;
     this.hasAggs = aggs && aggs.length > 0;
+    this.aggsGroups = [
+      {
+        id: 'aggs',
+        label: '',
+        items: (this.aggs || []).map((a) => ({
+          id: a.objectTypeId,
+          label: a.label || a.objectTypeId,
+          svgSrc: this.systemService.getObjectTypeIconUri(a.objectTypeId),
+          count: a.count,
+          value: a
+        }))
+      }
+    ];
   }
 
-  applyAggregation(agg: ObjectTypeAggregation) {
-    this.quickSearchEl?.applyTypeAggration(agg, true);
+  applyAggregation(item: Selectable) {
+    this.quickSearchEl?.applyTypeAggration(item.value as ObjectTypeAggregation, true);
   }
 
   ngOnInit() {
