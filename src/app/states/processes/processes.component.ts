eab0baa8e587c6ba6864f6b6a2b3f0b746bf33b5
Peter Krajnik <krajnik@optimal-systems.de>
2021-08-18 21:58:55 +0200

    quick fix: hide slave panel on deselect

--- src/app/states/processes/processes.component.ts
+++ src/app/states/processes/processes.component.ts
@@ -27,7 +27,6 @@ export class ProcessesComponent implements OnInit, OnDestroy {
   layoutOptionsKey = 'yuv.app.processes';
   contextError: string;
   objectDetailsID: string;
-  itemIsSelected = false;
   objectId: string;
   selectedProcess: any;
   processData$: Observable<ResponsiveTableData>;
@@ -63,10 +62,9 @@ export class ProcessesComponent implements OnInit, OnDestroy {
     );
   }
 
-  selectedItem(item) {
+  selectedItem(item: any[]) {
     this.selectedProcess = item;
-    this.objectId = item[0]?.documentId ? item[0]?.documentId : ProcessDefinitionKey.INVALID_TYPE;
-    this.itemIsSelected = true;
+    this.objectId = item[0] ? item[0]?.documentId || ProcessDefinitionKey.INVALID_TYPE : null;
   }
 
   refreshList() {
