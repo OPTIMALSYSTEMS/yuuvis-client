da48c8a1b8327c45a51990681d84682d27bb078b
Andreas Schulz <schulz@optimal-systems.de>
2021-08-18 09:05:54 +0200

    attachment list

--- projects/yuuvis/framework/src/lib/bpm/task-details/task-details-attachments/task-details-attachments.component.ts
+++ projects/yuuvis/framework/src/lib/bpm/task-details/task-details-attachments/task-details-attachments.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BaseObjectTypeField, ClientDefaultsObjectTypeField, SearchFilter, SearchFilterGroup, SearchQuery, SearchResult, SearchService } from '@yuuvis/core';
 
 @Component({
   selector: 'yuv-task-details-attachments',
@@ -6,9 +7,46 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./task-details-attachments.component.scss']
 })
 export class TaskDetailsAttachmentsComponent implements OnInit {
-  @Input() objectIDs: string[];
+  @Input() set objectIDs(oids: string[]) {
+    this.fetchAttachmentDetails(oids);
+  }
+  attachedObjects: { id: string; objectTypeId: string; title: string }[] = [];
+  selectedObject: string;
+  busy: boolean;
 
-  constructor() {}
+  constructor(private searchService: SearchService) {}
+
+  selectAttachment(id: string) {
+    this.selectedObject = id;
+  }
+
+  private fetchAttachmentDetails(oids: string[]) {
+    if (oids?.length) {
+      this.busy = true;
+      let query = new SearchQuery();
+      query.fields = [BaseObjectTypeField.OBJECT_ID, BaseObjectTypeField.OBJECT_TYPE_ID, ClientDefaultsObjectTypeField.TITLE];
+      let group = SearchFilterGroup.fromArray(oids.map((id) => new SearchFilter(BaseObjectTypeField.OBJECT_ID, SearchFilter.OPERATOR.EEQUAL, id)));
+      group.operator = SearchFilterGroup.OPERATOR.OR;
+      query.addFilterGroup(group);
+      this.searchService.search(query).subscribe(
+        (res: SearchResult) => {
+          this.attachedObjects = res.items.map((i) => ({
+            id: i.fields.get(BaseObjectTypeField.OBJECT_ID),
+            objectTypeId: i.fields.get(BaseObjectTypeField.OBJECT_TYPE_ID),
+            title: i.fields.get(ClientDefaultsObjectTypeField.TITLE)
+          }));
+          if (this.attachedObjects.length === 1) {
+            this.selectedObject = this.attachedObjects[0].id;
+          }
+          this.busy = false;
+        },
+        (err) => {
+          // TODO: handle error
+          this.busy = false;
+        }
+      );
+    }
+  }
 
   ngOnInit(): void {}
 }
