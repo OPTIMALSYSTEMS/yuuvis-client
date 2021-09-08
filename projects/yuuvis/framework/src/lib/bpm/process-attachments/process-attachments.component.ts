c22202c69b7cb4af65ef679a073010f4acb30b73
Andreas Schulz <schulz@optimal-systems.de>
2021-09-08 15:43:14 +0200

    review fixes

--- projects/yuuvis/framework/src/lib/bpm/process-attachments/process-attachments.component.ts
+++ projects/yuuvis/framework/src/lib/bpm/process-attachments/process-attachments.component.ts
@@ -110,7 +110,7 @@ export class ProcessAttachmentsComponent implements OnInit {
       this.searchService.search(query).subscribe(
         (res: SearchResult) => {
           // map result to maintain the correct order
-          const qa: any = {};
+          const qa: { [key: string]: any } = {};
           res.items.forEach((i) => {
             qa[i.fields.get(BaseObjectTypeField.OBJECT_ID)] = {
               id: i.fields.get(BaseObjectTypeField.OBJECT_ID),
