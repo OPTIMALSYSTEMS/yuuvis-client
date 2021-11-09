b513e16112a6d84754ed03db450c8c11a0d1ab65
Andreas Schulz <schulz@optimal-systems.de>
2021-11-09 06:59:26 +0100

    cell renderer for checkboxes and switches

--- projects/yuuvis/framework/src/lib/services/grid/grid.cellrenderer.ts
+++ projects/yuuvis/framework/src/lib/services/grid/grid.cellrenderer.ts
@@ -109,15 +109,23 @@ export class CellRenderer {
   }
 
   static booleanCellRenderer(param) {
-    let val = `<path class="null" d="M19,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V5C21,3.9,20.1,3,19,3z"/>`;
-
+    let cssValue = '';
     if (param.value === true || param.value === 'true') {
-      val = `<path class="outline" d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z"/>
-               <polyline class="checkmark" points="6.3,11.8 7.8,10.3 10.8,13.3 16.3,7.8 17.8,9.3 10.8,16.3 6.3,11.8"/>`;
+      cssValue = ' true';
     } else if (param.value === false || param.value === 'false') {
-      val = `<path class="outline" d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z"/>`;
+      cssValue = ' false';
     }
-    return `<div style="display:flex; align-items:center"><svg class="checkbox" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">${val}</svg></div>`;
+    return `<div class="yuv-cr-checkbox${cssValue}"></div>`;
+  }
+
+  static booleanSwitchCellRenderer(param) {
+    let cssValue = '';
+    if (param.value === true || param.value === 'true') {
+      cssValue = ' true';
+    } else if (param.value === false || param.value === 'false') {
+      cssValue = ' false';
+    }
+    return `<div class="yuv-cr-switch${cssValue}"></div>`;
   }
 
   static multiSelectCellRenderer(param: any) {
