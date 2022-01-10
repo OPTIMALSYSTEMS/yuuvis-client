9e7e3eae312ebd27930eca769a7d7d95de523abe
Andreas Schulz <schulz@optimal-systems.de>
2022-01-10 15:01:24 +0100

    BUD-682 all tags in history for admins

--- projects/yuuvis/core/src/lib/service/audit/audit.service.ts
+++ projects/yuuvis/core/src/lib/service/audit/audit.service.ts
@@ -66,7 +66,7 @@ export class AuditService {
 
     const auditActions = this.getAuditActions(!!options.allActions, options?.skipActions);
 
-    if (objectTypeId) {
+    if (objectTypeId && !this.userService.hasAdministrationRoles) {
       const vTags = this.system.getVisibleTags(objectTypeId);
       const tagAudits = [110, 210, 310];
       const actionFilterGroup: SearchFilterGroup = new SearchFilterGroup(SearchFilterGroup.DEFAULT, SearchFilterGroup.OPERATOR.AND, [
