62df983daa5df89d4f813a03f5dcaadba4ab0173
Andreas Schulz <schulz@optimal-systems.de>
2022-01-20 09:50:07 +0100

    Made ProcessDefinitionKey a const instead of enum to be more flexible starting processes

--- projects/yuuvis/core/src/lib/service/bpm/bpm/bpm.service.ts
+++ projects/yuuvis/core/src/lib/service/bpm/bpm/bpm.service.ts
@@ -65,11 +65,11 @@ export class BpmService {
     return this.backendService.get(url).pipe(finalize(() => setTimeout(() => this.loadingBpmDataSource.next(false), 200)));
   }
 
-  getProcessInstances(processDefinitionKey: ProcessDefinitionKey, options: FetchTaskOptions = { includeProcessVar: true }): Observable<Process[]> {
+  getProcessInstances(processDefinitionKey: string, options: FetchTaskOptions = { includeProcessVar: true }): Observable<Process[]> {
     return this.backendService.get(`${this.bpmProcessUrl}${this.optionsToParams({ ...options, processDefinitionKey })}`, ApiBase.apiWeb);
   }
 
-  getProcessInstance(processDefinitionKey: ProcessDefinitionKey, options?: FetchTaskOptions): Observable<Process> {
+  getProcessInstance(processDefinitionKey: string, options?: FetchTaskOptions): Observable<Process> {
     return this.backendService
       .get(`${this.bpmProcessUrl}${this.optionsToParams({ ...options, processDefinitionKey })}`, ApiBase.apiWeb)
       .pipe(map(({ objects }) => objects[0]));
