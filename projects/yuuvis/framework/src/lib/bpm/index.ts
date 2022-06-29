b7b1b0958bcfd5884a1514b44a8b034b5b718772
Peter Krajnik <krajnik@optimal-systems.de>
2022-06-29 22:55:32 +0200

    i18n merge

--- projects/yuuvis/framework/src/lib/bpm/index.ts
+++ projects/yuuvis/framework/src/lib/bpm/index.ts
@@ -1,6 +1,7 @@
 export * from './bpm.module';
 export { FollowUpDetailsComponent } from './follow-up-details/follow-up-details.component';
 export * from './interface/bpm.interface';
+export { ProcessAttachmentsOrderComponent } from './process-attachments/process-attachments-order/process-attachments-order.component';
 export { ProcessAttachmentsComponent } from './process-attachments/process-attachments.component';
 export { ProcessDetailsSummaryComponent } from './process-details/process-details-summary/process-details-summary.component';
 export { ProcessDetailsComponent } from './process-details/process-details.component';
@@ -8,6 +9,8 @@ export { ProcessListEmptyComponent } from './process-list-empty/process-list-emp
 export { ProcessListComponent } from './process-list/process-list.component';
 export { FormatProcessDataService } from './services/formatProcessData.service';
 export { TaskDetailsAttachmentsComponent } from './task-details/task-details-attachments/task-details-attachments.component';
+export { TaskDetailsCommentsComponent } from './task-details/task-details-comments/task-details-comments.component';
 export { TaskDetailsHistoryComponent } from './task-details/task-details-history/task-details-history.component';
+export { TaskDelegatePickerComponent } from './task-details/task-details-task/task-delegate-picker/task-delegate-picker.component';
 export { TaskDetailsTaskComponent } from './task-details/task-details-task/task-details-task.component';
 export { TaskDetailsComponent } from './task-details/task-details.component';
