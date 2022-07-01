cbf135aea4b1158999f26c926e7ad5cee351ffd4
Andreas Schulz <schulz@optimal-systems.de>
2022-07-01 13:25:56 +0200

    skipTypes for attachments on task details

--- projects/yuuvis/framework/src/lib/bpm/task-details/task-details-attachments/task-details-attachments.component.ts
+++ projects/yuuvis/framework/src/lib/bpm/task-details/task-details-attachments/task-details-attachments.component.ts
@@ -16,6 +16,8 @@ export class TaskDetailsAttachmentsComponent implements OnInit {
   }
 
   @Input() layoutOptionsKey: string;
+  // list of types (object type IDs) that should not be offered when adding new attachments
+  @Input() attachmentsSkipTypes: string[];
 
   @Input() plugins: any;
 
