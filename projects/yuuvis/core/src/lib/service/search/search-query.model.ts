20234ff5ac95830dc441bbc740774a817a5ba745
proki <krajnik@optimal-systems.de>
2022-08-02 18:47:34 +0000

    BUD-73 dynamic searchFilters

--- projects/yuuvis/core/src/lib/service/search/search-query.model.ts
+++ projects/yuuvis/core/src/lib/service/search/search-query.model.ts
@@ -265,12 +265,12 @@ export class SearchQuery {
     }
 
     if (this.filterGroup && !this.filterGroup.isEmpty()) {
-      const fg = this.filterGroup.toShortQuery();
+      const fg = this.filterGroup.toShortQuery(combineFilters);
       queryJson.filters = fg.filters.length > 1 && this.filterGroup.operator === SearchFilterGroup.OPERATOR.OR ? [fg] : fg.filters;
     }
 
     if (this.hiddenFilterGroup && !this.hiddenFilterGroup.isEmpty()) {
-      const fg = this.hiddenFilterGroup.toShortQuery();
+      const fg = this.hiddenFilterGroup.toShortQuery(combineFilters);
       const filters = fg.filters.length > 1 && this.hiddenFilterGroup.operator === SearchFilterGroup.OPERATOR.OR ? [fg] : fg.filters;
       if (combineFilters) {
         queryJson.filters = filters.concat(queryJson.filters || []);
@@ -405,7 +405,7 @@ export class SearchFilterGroup {
     };
   }
 
-  toShortQuery() {
+  toShortQuery(resolved = false) {
     return {
       ...(this.property !== SearchFilterGroup.DEFAULT ? { property: this.property } : {}),
       ...(this.operator !== SearchFilterGroup.OPERATOR.AND ? { lo: this.operator } : {}),
@@ -414,9 +414,9 @@ export class SearchFilterGroup {
         .map((g) =>
           g instanceof SearchFilterGroup
             ? g.filters.filter((f) => !f.excludeFromQuery).length === 1
-              ? g.filters.filter((f) => !f.excludeFromQuery)[0].toQuery()
-              : g.toShortQuery()
-            : g.toQuery()
+              ? g.filters.filter((f) => !f.excludeFromQuery)[0][resolved ? 'toResolvedQuery' : 'toQuery']()
+              : g.toShortQuery(resolved)
+            : g[resolved ? 'toResolvedQuery' : 'toQuery']()
         )
     };
   }
@@ -476,6 +476,29 @@ export class SearchFilter {
     RANGE: '=' // aggegation ranges
   };
 
+  /**
+   * available variables for a search filter
+   */
+   public static VARIABLES = {
+    CURRENT_USER: '$CURRENT_USER$',
+    NOW: '$NOW$',
+    TODAY: '$TODAY$',
+    YESTERDAY: '$YESTERDAY$',
+    THISWEEK: '$THISWEEK$',
+    THISMONTH: '$THISMONTH$',
+    THISYEAR: '$THISYEAR$',
+  };
+
+  public static parseVariable(value: string): { value: string, key: string, base: string, operator: string } {
+    return Object.values(SearchFilter.VARIABLES).includes(value?.replace && value.replace(new RegExp('\\|.*'), '')) 
+      && {
+        value,
+        key: value?.replace(new RegExp('\\$', 'g'), '').replace(new RegExp('\\|.*'), ''),
+        base: value?.replace(new RegExp('\\|.*'), ''),
+        operator: value?.replace(new RegExp('.*\\|'), '') || SearchFilter.OPERATOR.EQUAL
+      };
+  }
+
   public static fromQuery(query: any) {
     return new SearchFilter(query.f, query.o, query.v1, query.v2, query.useNot);
   }
@@ -519,6 +542,29 @@ export class SearchFilter {
 
   isEmpty() {
     return Utils.isEmpty(this.firstValue) || (this.operator?.match(/gt(e)?lt(e)?/) ? Utils.isEmpty(this.secondValue) : false);
+  }
+
+  toResolvedQuery() {
+    const query = this.toQuery();
+    query.v1 = this.resloveValue(query.v1);
+    query.v2 = this.resloveValue(query.v2);
+    return query;
+  }
+
+  resloveValue(value: any) {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    switch(SearchFilter.parseVariable(value)?.base) {
+      case SearchFilter.VARIABLES.CURRENT_USER: return window['api'].session.user.get().id;
+      case SearchFilter.VARIABLES.NOW: return new Date().toISOString();
+      case SearchFilter.VARIABLES.TODAY: return date.toISOString();
+      case SearchFilter.VARIABLES.YESTERDAY: return date.setHours(-24) && date;
+      case SearchFilter.VARIABLES.THISWEEK: return  date.setHours(-24 * 7) && date;
+      case SearchFilter.VARIABLES.THISMONTH: return date.setMonth(date.getMonth() - 1) && date;
+      case SearchFilter.VARIABLES.THISYEAR: return date.setFullYear(date.getFullYear() - 1) && date;
+    }
+    return value;
   }
 
   toQuery() {
--- projects/yuuvis/framework/src/lib/form/form-input/form-input.component.ts
+++ projects/yuuvis/framework/src/lib/form/form-input/form-input.component.ts
@@ -13,6 +13,7 @@ import {
   ViewChildren
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { SearchFilter, TranslateService } from '@yuuvis/core';
 import { PluginsService } from '../../plugins/plugins.service';
 import { PluginTriggerComponent } from './../../plugins/plugin-trigger.component';
 
@@ -68,6 +69,10 @@ export class FormInputComponent implements AfterViewInit {
     this.toggled = n;
   }
 
+  @Input() variable: any;
+
+  variables = SearchFilter.VARIABLES;
+
   /**
    * Indicator that the wrapped form element is invalid. Will then render appropriate styles.
    * You may also provide an array of error messages.
@@ -105,7 +110,7 @@ export class FormInputComponent implements AfterViewInit {
   /**
    * Emits whether or not the input was set to 'Not set' state. Requires input `skipToggle` to be false.
    */
-  @Output() onToggleLabel = new EventEmitter<boolean>();
+  @Output() onToggleLabel = new EventEmitter<{toggled: boolean, variable: any}>();
 
   @HostBinding('class.disabled') isDisabled;
   @HostBinding('class.invalid') isInvalid;
@@ -125,13 +130,48 @@ export class FormInputComponent implements AfterViewInit {
 
   visiblePlugins = [];
   hiddenPlugins = [];
+  variablePlugins = [
+    {
+      "id": "yuv.framework.trigger.filter.variable.user",
+      "label": "yuv.framework.trigger.filter.variable.user",
+      "matchHook": "yuv-organization",
+      "group": "visible",
+      "icon": "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" aria-hidden=\"true\" focusable=\"false\" width=\"1em\" height=\"1em\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 24 24\"><path opacity=\".3\" d=\"M17 7H7V4H5v16h14V4h-2z\" fill=\"white\"/><path d=\"M19 2h-4.18C14.4.84 13.3 0 12 0S9.6.84 9.18 2H5c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1s-1-.45-1-1s.45-1 1-1zm7 18H5V4h2v3h10V4h2v16z\" fill=\"#626262\"/></svg>",
+      "isExecutable": "(component) => component.parent.formControlName",
+      "run": "(component) => { component.parent.toggle(true, component.parent.variables['CURRENT_USER']); }"
+    },
+    {
+      "id": "yuv.framework.trigger.filter.variable.date",
+      "label": "yuv.framework.trigger.filter.variable.date",
+      "matchHook": "yuv-datetime",
+      "group": "visible",
+      "icon": "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" aria-hidden=\"true\" focusable=\"false\" width=\"1em\" height=\"1em\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 24 24\"><path opacity=\".3\" d=\"M17 7H7V4H5v16h14V4h-2z\" fill=\"white\"/><path d=\"M19 2h-4.18C14.4.84 13.3 0 12 0S9.6.84 9.18 2H5c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1s-1-.45-1-1s.45-1 1-1zm7 18H5V4h2v3h10V4h2v16z\" fill=\"#626262\"/></svg>",
+      "isExecutable": "(component) => component.parent.formControlName && component.parent.childComponent.searchOption !== 'gtelte'",
+      "run": "(component) => { var values = Object.keys(component.parent.variables).filter(c => !c.match('CURRENT_USER')); var map = {}; values.forEach((v, i) => { map[v]= values[i+1]; }); map[values[values.length - 1]] = values[0]; component.parent.toggle(true, component.parent.variables[map[component.parent.parseVariable(component.parent.variable).key || values[values.length - 1]]] + '|' + (component.parent.childComponent.searchOption)); }"
+    }
+  ];
 
-  constructor(private renderer: Renderer2, private elRef: ElementRef, private pluginsService: PluginsService) {}
+  constructor(private renderer: Renderer2, private elRef: ElementRef, private pluginsService: PluginsService, private translate: TranslateService) {
+    this.translate.instant('yuv.framework.trigger.filter.variable.user');
+    this.translate.instant('yuv.framework.trigger.filter.variable.date');
+  }
 
-  toggle() {
+  parseVariable(variable: string) {
+    return SearchFilter.parseVariable(variable) || {} as any;
+  }
+
+  variableTitle(variable: string) {
+    const op = this.childComponent.searchOption = this.parseVariable(variable).operator || SearchFilter.OPERATOR.EQUAL;
+    const labels = { [SearchFilter.OPERATOR.GREATER_OR_EQUAL] : SearchFilter.OPERATOR_LABEL.GREATER_OR_EQUAL, [SearchFilter.OPERATOR.LESS_OR_EQUAL] : SearchFilter.OPERATOR_LABEL.LESS_OR_EQUAL };
+    return variable === SearchFilter.VARIABLES.CURRENT_USER ? window['api'].session.user.get().title : 
+        (labels[op] || '') + this.pluginsService.translate.instant(`yuv.framework.search.agg.time.${this.parseVariable(variable).key?.toLowerCase()}`);
+  }
+
+  toggle(toggle = !this.toggled, variable = null) {
     if (!this.skipToggle && !this.isDisabled) {
-      this.toggled = !this.toggled;
-      this.onToggleLabel.emit(this.toggled);
+      this.toggled = toggle;
+      this.variable = variable;
+      this.onToggleLabel.emit({ toggled: this.toggled, variable });
     }
   }
 
@@ -151,6 +191,9 @@ export class FormInputComponent implements AfterViewInit {
     this.pluginsService.getCustomPlugins('triggers', this.hook).subscribe((t) => {
       this.visiblePlugins = t.filter((action: any) => action.group === 'visible');
       this.hiddenPlugins = t.filter((action: any) => action.group !== 'visible');
+      const variable = this.variablePlugins.find(v => this.hook.match(v.matchHook));
+      if (variable && !this.visiblePlugins.find(v => v.id === variable.id))
+        this.visiblePlugins = [variable, ...this.visiblePlugins];
     });
   }
 }
