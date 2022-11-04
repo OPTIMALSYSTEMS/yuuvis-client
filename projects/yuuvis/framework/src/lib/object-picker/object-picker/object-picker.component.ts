abb7e02237b94371d0eea5e5602bc6be4e9843c7
Andreas Schulz <schulz@optimal-systems.de>
2022-11-04 11:46:14 +0100

    BUD-1920 skipTypes in object picker

--- projects/yuuvis/framework/src/lib/object-picker/object-picker/object-picker.component.ts
+++ projects/yuuvis/framework/src/lib/object-picker/object-picker/object-picker.component.ts
@@ -1,6 +1,6 @@
 import { FocusKeyManager } from '@angular/cdk/a11y';
 import { Component, EventEmitter, HostListener, Input, OnInit, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { BaseObjectTypeField, SearchQuery, SearchResult, SearchService, SystemService } from '@yuuvis/core';
+import { BaseObjectTypeField, SearchFilter, SearchQuery, SearchResult, SearchService, SystemService } from '@yuuvis/core';
 import { Selectable } from '../../grouped-select/grouped-select/grouped-select.interface';
 import { SelectableItemComponent } from '../../grouped-select/grouped-select/selectable-item/selectable-item.component';
 import { PopoverRef } from '../../popover/popover.ref';
@@ -46,6 +46,11 @@ export class ObjectPickerComponent implements OnInit {
     if (q.filterGroup.filters.length || q.term?.length || q.types.length || q.lots.length) {
       q.fields = [BaseObjectTypeField.OBJECT_ID, BaseObjectTypeField.OBJECT_TYPE_ID, bp.title, bp.description];
       q.size = 10;
+
+      if (this.skipTypes?.length) {
+        q.addFilter(new SearchFilter(BaseObjectTypeField.LEADING_OBJECT_TYPE_ID, SearchFilter.OPERATOR.IN, this.skipTypes, null, true));
+      }
+
       this.loading = true;
       this.searchService.search(q).subscribe(
         (res: SearchResult) => {
